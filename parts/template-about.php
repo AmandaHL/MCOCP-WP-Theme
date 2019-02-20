782fd679340ffe1e409419dcba23296fd830ad28
Amanda Long <amanda@designformation.co>
2019-02-20 12:27:16 -0700

    Update template-about.php
    Updated to reflect latest version

--- parts/template-about.php
+++ parts/template-about.php
@@ -2,18 +2,15 @@
 	<?php
  
 		// Retrieves the stored value from the database
+		$btnTxtBrochure = get_post_meta(get_the_ID(), 'mcocp_btn_txt_brochure', true);
+		$btnLinkBrochure = get_post_meta(get_the_ID(), 'mcocp_btn_link_brochure', true);
 		$donorsTxt = wpautop( get_post_meta( get_the_ID(), 'mcocp_donors_txt', true) );
 		$btnTxtDonors = get_post_meta(get_the_ID(), 'mcocp_btn_txt_donors', true);
 		$btnLinkDonors = get_post_meta(get_the_ID(), 'mcocp_btn_link_donors', true);
 		
 		$logos = get_post_meta(get_the_ID(), 'mcocp_logos', true);
-
-		// Checks and displays the retrieved value
-		if( !empty( $bannerImage ) ) {
-			 echo '<div class="home-banner"><img src="'.$bannerImage.'"></div><!--.home-banner-->';
-		}
-
-			
+		$btnTxtMentor = get_post_meta(get_the_ID(), 'mcocp_btn_txt_a_mentor', true);
+		$btnLinkMentor = get_post_meta(get_the_ID(), 'mcocp_btn_link_a_mentor', true);
 	
 	?>
 		<!-- section -->
@@ -26,12 +23,13 @@
 						<div id="post-<?php the_ID(); ?>" class="content">
 
 							<?php the_content(); ?>
-
+							<a class="button" href="<?php echo $btnLinkBrochure; ?>" target="_blank"><?php echo $btnTxtBrochure; ?></a>
 						</div><!--.content -->
 
 					<?php endwhile; ?>
 
 					<?php endif; ?>
+					
 				</div>
 			</section>
 		<!-- /section -->
@@ -56,6 +54,7 @@
 				<div>
 					<div class="content">
 						<?php get_template_part('parts/logos');?>
+						<a class="button" href="<?php echo $btnLinkMentor; ?>"><?php echo $btnTxtMentor; ?></a>
 					</div>
 				</div>
 			</section>
